0d7ff71c91cc9a003e6c778ee1d8b54d420482d8
l4279625 <38735150+l4279625@users.noreply.github.com>
2018-10-20 19:48:56 +0900

    Add files via upload

---
+++ js/history.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function() {
+    $(".history_1").mouseenter(function() {
+        $('.history_main').attr("src", "img/history/1.jpg");
+    });
+    $(".history_2").mouseenter(function() {
+        $('.history_main').attr("src", "img/history/2.jpg");
+    });
+    $(".history_3").mouseenter(function() {
+        $('.history_main').attr("src", "img/history/3.jpg");
+    });
+});
